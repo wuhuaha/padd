cb528b8e73aa8d508bdf2bab3fb294cff99f86f1
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-05 02:13:23 +0000

    Adding numeric_owner as keyword arguement

--- paddlepalm/utils/saver.py
+++ paddlepalm/utils/saver.py
@@ -73,7 +73,7 @@ def init_pretraining_params(exe,
                     if not is_within_directory(path, member_path):
                         raise Exception("Attempted Path Traversal in Tar File")
             
-                tar.extractall(path, members, numeric_owner) 
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
                 
             
             safe_extract(f, os.path.join(pretraining_params_path,".temp"))
